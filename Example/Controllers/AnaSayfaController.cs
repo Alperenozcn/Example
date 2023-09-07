5af5e9eeac0971a6ec2b8d70b837a4274e9da617
Alperen <129869740+Alperenozcn@users.noreply.github.com>
2023-09-07 23:47:29 +0300

    Add project files.

---
+++ Example/Controllers/AnaSayfaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.Controllers
+{
+    public class AnaSayfaController : Controller
+    {
+        // GET: AnaSayfa
+        public ActionResult Sevgul()
+        {
+            return View();
+        }
+
+        public ActionResult Alperen()
+        {
+            return View("Alperen");
+        }
+
+        public ActionResult yavuz()
+        {
+            return View();
+        }
+        public ActionResult Alperen(int id, string name)
+        {
+            //var yavuz = HttpContext.request.querystring["yavuz"];
+            return View();
+        }
+    }
+}
---
+++ Example/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Product> products;
+
+        public HomeController() 
+        {
+            products = new List<Product>
+            {
+                new Product(){Name = "Iphone 14", Price = 75000},
+                new Product(){Name = "Samsung S23",Price= 40000}
+            };
+
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}

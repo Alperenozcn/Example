9bf74ee5540288ef9e0bbd6dd15ca1d07347bd2f
Alperen <129869740+Alperenozcn@users.noreply.github.com>
2023-09-21 20:42:04 +0300

    Cv_QueryFirst

--- Example/Controllers/HomeController.cs
+++ Example/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using Example.Models;
+﻿using Dapper;
+using Example.Entities;
+using Example.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,26 +12,75 @@ namespace Example.Controllers
 {
     public class HomeController : Controller
     {
-        private List<Product> products;
-
-        public HomeController() 
-        {
-            products = new List<Product>
-            {
-                new Product(){Name = "Iphone 14", Price = 75000},
-                new Product(){Name = "Samsung S23",Price= 40000}
-            };
-
-        }
-        // GET: Home
         public ActionResult Index()
         {
-            return View(products);
+            SqlConnection connection = new SqlConnection("server=ALPEREN\\ALPERENMSSQL; database=CvDb; integrated security=true");
+
+            var about = connection.QueryFirst<About>(sql: "select * from About");
+
+
+            //SqlCommand command = new SqlCommand();
+            //command.Connection = connection;
+            //command.CommandType = System.Data.CommandType.Text;
+            //command.CommandText = "select * from About where Id = 1";
+
+            //connection.Open();
+            //var reader = command.ExecuteReader();
+            //About about = new About();
+            //while (reader.Read())
+            //{
+            //    about.Id = reader.GetInt32(0);
+            //    about.Fullname = reader.GetString(1);
+            //    about.JobTitle = reader.GetString(2);
+            //    about.Description = reader.GetString(3);
+            //    about.ImagePath = reader.IsDBNull(4)?"userprofile.jpg": reader.GetString(4);
+            //    about.LınkedIn = reader.GetString(5);
+            //    about.Gıt = reader.GetString(6);
+            //    about.Instagram = reader.GetString(7);
+            //    about.Twitter = reader.GetString(8);
+            //}
+
+            //connection.Close();
+            //reader.Close();
+
+            //var dataSingle = connection.QuerySingle(sql: "select * from Product");
+
+            return View(about);
         }
 
+        public ActionResult Portfolio()
+        {
+            SqlConnection connection = new SqlConnection("server=ALPEREN\\ALPERENMSSQL; database=CvDb; integrated security=true");
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.CommandText = "ap_ListSlogan";
+            command.Parameters.Add("@sectionName","Skills");
+
+            connection.Open();
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var title = reader.GetString(2);
+                var description = reader.GetString(3);
+            }
+            connection.Close();
+            reader.Close();
+            return View();
+        }
         public ActionResult Contact()
         {
             return View();
+        }
+
+        public string GetFirstElementName(List<Product>products)
+        {
+            return products[0].Name;
+        }
+
+        public string GetSingleElementName(Product product)
+        {
+            return product.Name;
         }
     }
 }
---
+++ Example/Entities/About.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Entities
+{
+    public class About
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string JobTitle { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string LınkedIn { get; set; }
+        public string Gıt { get; set; }
+        public string Instagram { get; set; }
+        public string Twitter { get; set;}
+        public string Stackoverflow { get; set;}
+  
+
+    }
+}
